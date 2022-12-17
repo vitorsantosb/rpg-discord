e452911e87668cf1c796d04c1f19b5bb0fa18cf9
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2022-12-17 16:00:25 -0300

    eslLite install and update init js

--- src/Discord/init.js
+++ src/Discord/init.js
@@ -2,15 +2,48 @@ const { Client, Events, GatewayIntentBits } = require("discord.js");
 require('dotenv').config();
 const token = process.env.TOKEN;
 
+const fs = require('node:fs');
+const path = require('node:path');
+
+
 async function discordInitialize() {
 
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
     const commands = new Map();
 
-    const preCommands = [
-        require('./command/ping'),
-    ];
+    client.commands = new Collection();
+
+    const commandsPath = path.join(__dirname, 'commands');
+
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+    
+    client.on(Events.InteractionCreate, async interaction => {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    });
 
     client.once(Events.ClientReady, c => {
         console.log(`Ready! Logged in as ${c.user.tag}`);
