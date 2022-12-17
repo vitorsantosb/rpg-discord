8ff70d2cb1d4f0429b976144e3789a4a38b26f9e
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2022-12-17 02:41:32 -0300

    update html

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { discordInitialize } = require("./src/Discord/init");
+const { init_routes } = require("./src/server/server");
 
 async function turnOn() {
+    
+    await init_routes();
     await discordInitialize();
+    
 }
 
 turnOn();
