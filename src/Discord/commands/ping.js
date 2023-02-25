14363f9bedbeea45d57253e79a63ceb49c0ba9ea
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-02-25 00:46:29 -0300

    Update ping.js and init

--- src/Discord/commands/ping.js
+++ src/Discord/commands/ping.js
@@ -1,8 +1,10 @@
-import { SlashCommandBuilder } from 'discord.js';
+const { SlashCommandBuilder } = require('discord.js');
 
-export const data = new SlashCommandBuilder()
-	.setName('ping')
-	.setDescription('Replies with Pong!');
-export async function execute(interaction) {
-	await interaction.reply('Pong!');
-}
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
--- src/Discord/init.js
+++ src/Discord/init.js
@@ -5,17 +5,25 @@ const token = process.env.TOKEN;
 const fs = require('node:fs');
 const path = require('node:path');
 
-async function getCommandList() {
-    const commandsPath = path.join(__dirname, "commands");
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-    console.log(commandFiles);
-}
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
 
 async function initDiscord() {
 
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
     
-    await getCommandList();
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        
+        
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 
     client.once(Events.ClientReady, c => {
         console.log(`Ready! Logged in as ${c.user.tag}`);
