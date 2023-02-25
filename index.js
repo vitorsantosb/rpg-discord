eda46485376b090b6147212e0c9bfa2a657056d2
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-02-25 00:30:26 -0300

    create command map

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { discordInitialize } = require("./src/Discord/init");
+const { initDiscord } = require("./src/Discord/init");
 const { initDatabase } = require("./src/server/database/database");
 const { init_routes } = require("./src/server/server");
 
 async function init() {
     
     await init_routes();
-    await discordInitialize();
-    await initDatabase();
+    await initDiscord();
+    //await initDatabase();
 }
 
 init();
--- src/Discord/init.js
+++ src/Discord/init.js
@@ -5,28 +5,17 @@ const token = process.env.TOKEN;
 const fs = require('node:fs');
 const path = require('node:path');
 
+async function getCommandList() {
+    const commandsPath = path.join(__dirname, "commands");
+    const commandFiles = fs.readFileSync(commandsPath).filter(file => file.endsWith(".js"));
+    console.log(commandFiles);
+}
 
-async function discordInitialize() {
+async function initDiscord() {
 
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-    const commands = new Map();
-
-    client.on(Events.InteractionCreate, async interaction => {
-        const command = interaction.client.commands.get(interaction.commandName);
-
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
     
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    });
+    await getCommandList();
 
     client.once(Events.ClientReady, c => {
         console.log(`Ready! Logged in as ${c.user.tag}`);
@@ -35,14 +24,7 @@ async function discordInitialize() {
     client.login(token);
 }
 
+module.exports = { initDiscord }
 
-module.exports = { 
-    name: "discord_init",
-
-    execute: async function(){
-        console.log("[Discord] Initialize Discord Script");
-        await discordInitialize();
-    }
-};
 
 
