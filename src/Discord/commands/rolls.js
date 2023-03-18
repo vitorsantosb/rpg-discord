88c7ffc8b37307d300da613717aeaf2be2f22853
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-03-18 00:18:24 -0300

    Update rolls.js

--- src/Discord/commands/rolls.js
+++ src/Discord/commands/rolls.js
@@ -15,20 +15,37 @@ module.exports = {
                     { name: 'd6', value: '6' },
                     { name: 'd4', value: '4' },
                 ))
-        .addNumberOption(numberOption => 
-            numberOption.setName('increment')
-            .setRequired(false)
-            .setDescription('Increment value for you dice')),
-        
+        .addIntegerOption(option => option.setName('modifier').setDescription('Add modifier for your dice!').setRequired(false))
+        .addIntegerOption(option => option.setName('rolls').setDescription('Number of rolls for your dice. Ex: 2d20, 2d10').setRequired(false))
+        .addIntegerOption(option => option.setName('min_value').setDescription('Minimal value for get a sucess in your dice!').setRequired(false)),
+
     async execute(interaction) {
         const dice = interaction.options.getString('dices');
-        const increment = interaction.options.getString('increment');
-        
-        const result = Math.floor(Math.random(dice) + 1);
+        const modifier = interaction.options.getInteger('modifier');
+        const rolls = interaction.options.getInteger('rolls');
+        const minValue = interaction.options.getInteger('min_value');
 
-        if(increment >= 0){
-            result += increment;
+
+        let results = [];
+
+        if (rolls == 0) {
+            rolls = 1;
         }
-        await interaction.reply(result);
+
+        for (let i = 0; i <= rolls; i++) {
+            let result = Math.floor(Math.random() * dice + 1);
+
+            if (modifier > 0) result += modifier;
+            
+            if (minValue != null) {
+                console.log(minValue);
+                if (result >= minValue) {
+                    results.push(" | " + result + " | ")
+                }
+            } else {
+                results.push(" | " + result + " | ")
+            }
+        }
+        await interaction.reply("Dice result: " + results);
     },
 }
