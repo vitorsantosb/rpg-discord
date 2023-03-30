27c46848efebd8478a46009e8f7823c9123dfae0
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-03-30 18:22:25 -0300

    Update deleteUser.js
    Needed hot fix on function DeleteUserById()

--- src/Discord/commands/deleteUser.js
+++ src/Discord/commands/deleteUser.js
@@ -12,33 +12,51 @@ function SaveDatabase(db) {
     fs.writeFile('data.json', JSON.stringify(db, null, 2));
 }
 
-async function DeleteUserDataById(user_id) {
-    const rows = GetDatabase();
+async function FetchUserDataById(user_id) {
+	const rows = GetDatabase();
 
-    for(let i = 0; i < rows.lenght; i++){
-        if(rows[i].user.id == user_id){
-            rows[i].splice(i, 1);
+	for (const row of rows) {
+		if (row.user.id == user_id) {
+			console.log("User Found!")
+			return true;
+		}
+	}
+}
+
+async function DeleteUserById(user_id) {
+    let rows = GetDatabase();
+    console.log("teste");
+    for(let index = 0; index <= rows.lenght; index++){
+       
+        if(rows[index].user.id == user_id){
+            rows = rows.splice(index, 1);
+            console.log(rows);
+            
+            SaveDatabase(rows);
         }
     }
 
-    SaveDatabase(rows);
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('delete')
         .setDescription('delete user with metion')
-        .addMentionableOption(option => 
+        .addMentionableOption(option =>
             option.setName('user')
-              .setDescription('The user you want to say hello to.')
-              .setRequired(true)),
+                .setDescription('The user you want to say hello to.')
+                .setRequired(true)),
 
     async execute(interaction) {
         const user = interaction.options.getMentionable('user');
         const user_id = user.id;
-
-        await DeleteUserDataById(user_id);
-        interaction.reply(`User: ${user.username} has been deleted by ${interaction.user.username}!`);
+        
+        if(FetchUserDataById(user_id)){
+            await DeleteUserById(user_id);
+            interaction.reply(`User: ${user.username + "#" + user.discriminator} has been deleted by ${interaction.user.username + "#" + interaction.user.discriminator}!`);
+            return;
+        }
+        interaction.reply('User not found');
 
     },
 };
