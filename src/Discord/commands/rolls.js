011628912caebf43a81d1a92524fd4f92a17467e
rodrigorhas <rodrigo@edinheiro.org>
2023-04-02 00:32:33 -0300

    add: mongodb client add: database bootstrap file updte: update user repository to mongodb

--- src/Discord/commands/rolls.js
+++ src/Discord/commands/rolls.js
@@ -1,50 +1,56 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const { SaveDiceHistory } = require("../repositories/user.repository");
+
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('roll')
-        .setDescription('Rolagem de dados!')
-        .addStringOption(option =>
-            option.setName('dices')
-                .setRequired(true)
-                .setDescription('Select the dice')
-                .addChoices(
-                    { name: 'd20', value: '20' },
-                    { name: 'd12', value: '12' },
-                    { name: 'd10', value: '10' },
-                    { name: 'd6', value: '6' },
-                    { name: 'd4', value: '4' },
-                ))
-        .addIntegerOption(option => option.setName('modifier').setDescription('Add modifier for your dice!').setRequired(false))
-        .addIntegerOption(option => option.setName('rolls').setDescription('Number of rolls for your dice. Ex: 2d20, 2d10').setRequired(false))
-        .addIntegerOption(option => option.setName('min_value').setDescription('Minimal value for get a sucess in your dice!').setRequired(false)),
+	data: new SlashCommandBuilder()
+		.setName("roll")
+		.setDescription("Rolagem de dados!")
+		.addStringOption(option =>
+			option.setName("dices")
+				.setRequired(true)
+				.setDescription("Select the dice")
+				.addChoices(
+					{ name: "d20", value: "20" },
+					{ name: "d12", value: "12" },
+					{ name: "d10", value: "10" },
+					{ name: "d6", value: "6" },
+					{ name: "d4", value: "4" },
+				))
+		.addIntegerOption(option => option.setName("modifier").setDescription("Add modifier for your dice!").setRequired(false))
+		.addIntegerOption(option => option.setName("rolls").setDescription("Number of rolls for your dice. Ex: 2d20, 2d10").setRequired(false))
+		.addIntegerOption(option => option.setName("min_value").setDescription("Minimal value for get a sucess in your dice!").setRequired(false)),
 
-    async execute(interaction) {
+	async execute(interaction) {
 
-        const dice = interaction.options.getString('dices');
-        const modifier = interaction.options.getInteger('modifier');
-        const rolls = interaction.options.getInteger('rolls');
-        const minValue = interaction.options.getInteger('min_value');
+		const dice = interaction.options.getString("dices");
+		const modifier = interaction.options.getInteger("modifier");
+		const rollsCount = interaction.options.getInteger("rolls") ?? 0;
+		const minValue = interaction.options.getInteger("min_value");
 
-        let results = [];
+		const rolls = [];
 
-        if (rolls == 0) {
-            rolls = 1;
-        }
+		for (let i = 0; i < rollsCount; i++) {
+			let result = Math.floor(Math.random() * dice);
 
-        for (let i = 0; i <= rolls; i++) {
-            let result = Math.floor(Math.random() * dice + 1);
+			if (modifier > 0) result += modifier;
 
-            if (modifier > 0) result += modifier;
+			if (minValue) {
+				if (result >= minValue) {
+					rolls.push(result);
+				}
+			} else {
+				rolls.push(result);
+			}
+		}
 
-            if (minValue != null) {
-                if (result >= minValue) {
-                    results.push(" | " + result + " | ")
-                }
-            } else {
-                results.push(" | " + result + " | ")
-            }
-        }
-        await interaction.reply("Dice result: " + results);
-    },
-}
+		const rollsAsText = rolls.join(" | ");
+
+		await SaveDiceHistory(interaction.user.id, {
+			type: "d", size: dice,
+			rolls, rollsAsText
+		});
+
+		await interaction.reply("Dice result: " + rollsAsText);
+	},
+};
--- src/Discord/init.js
+++ src/Discord/init.js
@@ -1,65 +1,65 @@
 const { Client, Events, GatewayIntentBits, Collection, Message } = require("discord.js");
-require('dotenv').config();
+require("dotenv").config();
 const token = process.env.TOKEN;
 
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require("node:fs");
+const path = require("node:path");
 
 function loadCommands(client){
     
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandsPath = path.join(__dirname, "commands");
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 async function initCommands() {
     
-    const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-    client.commands = new Collection();
+	const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+	client.commands = new Collection();
     
-    loadCommands(client);
+	loadCommands(client);
    
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
+	client.on(Events.InteractionCreate, async interaction => {
+		if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
+		const command = interaction.client.commands.get(interaction.commandName);
     
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
     
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+			} else {
+				await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+			}
+		}
         
-    });
+	});
 
-    client.login(token);
+	client.login(token);
 
-    client.once(Events.ClientReady, c => {
-        console.log(`Ready! Logged in as ${c.user.tag}`);
-    });
+	client.once(Events.ClientReady, c => {
+		console.log(`Ready! Logged in as ${c.user.tag}`);
+	});
 }
 
-module.exports = { initCommands }
+module.exports = { initCommands };
 
 
 
