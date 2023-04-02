8190d0ebe0eb50525da67b8b5dabbccf5247e423
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-04-02 01:16:29 -0300

    Create createSession.js

---
+++ src/Discord/commands/createSession.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('create_session')
+        .setDescription('Create a RPG session'),
+
+    async execute(interaction){
+        await interaction.reply('creating....');
+    }
+}
