39b409f2c3b14f8548ebedfd122bdaada84fa8d7
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-04-03 10:49:52 -0300

    Update: update initCommands function

--- src/Discord/init.js
+++ src/Discord/init.js
@@ -21,6 +21,20 @@ function loadCommands(client){
 		}
 	}
 }
+async function LoadEvents(client){
+	const eventsPath = path.join(__dirname, 'events');
+	const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of eventFiles) {
+		const filePath = path.join(eventsPath, file);
+		const event = require(filePath);
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+}
 
 async function initCommands() {
     
@@ -28,35 +42,9 @@ async function initCommands() {
 	client.commands = new Collection();
     
 	loadCommands(client);
-   
-	client.on(Events.InteractionCreate, async interaction => {
-		if (!interaction.isChatInputCommand()) return;
-
-		const command = interaction.client.commands.get(interaction.commandName);
-    
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
-    
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-			}
-		}
-        
-	});
+	LoadEvents(client);
 
 	client.login(token);
-
-	client.once(Events.ClientReady, c => {
-		console.log(`Ready! Logged in as ${c.user.tag}`);
-	});
 }
 
 module.exports = { initCommands };
