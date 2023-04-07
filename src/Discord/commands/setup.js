ef90e422e0da2599dddad9c735f297f1998594da
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-04-07 10:37:48 -0300

    update: Lint

--- src/Discord/commands/setup.js
+++ src/Discord/commands/setup.js
@@ -1,27 +1,34 @@
-const { SlashCommandBuilder, PermissionsBitField} = require('discord.js');
-const {FetchRoleInGuild, CreateRole, AssignRoleToUser} = require('../repositories/roleManager.repository');
+const {SlashCommandBuilder, PermissionsBitField} = require('discord.js');
+const {ExistsRoleInGuild, CreateRole, AssignRoleToUser} = require('../repositories/roleManager.repository');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('setup')
 		.setDescription('Receive a role for manager the bot - Obs: This function is enabled for server owner'),
 
-	execute: async function(interaction){
-		const { user, guild } = interaction;
-		const role_name = 'RPG-Admin';
-		const args = [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.KickMembers];
+	execute: async function (interaction) {
+		const {user, guild} = interaction;
+		const roleName = 'RPG-Admin';
 
-		if(guild.ownerId === user.id){
-			if(await FetchRoleInGuild(guild, role_name)){
-				interaction.reply(`You needed delete the role ${role_name}, for continue this command`);
+		const permissions = [
+			PermissionsBitField.Flags.SendMessages,
+			PermissionsBitField.Flags.KickMembers
+		];
+
+		if (guild.ownerId === user.id) {
+
+			if (ExistsRoleInGuild(guild, roleName)) {
+				interaction.reply(`You needed delete the role ${roleName}, for continue this command`);
 				return;
 			}
-			await CreateRole(guild, role_name, args, '#1965E9');
-			await AssignRoleToUser(interaction, role_name);
 
-			interaction.reply(`Setup Complete. The server owner ${user.username} receive the role ${role_name}`);
+			await CreateRole(guild, roleName, permissions, '#1965E9');
+			await AssignRoleToUser(interaction, roleName);
+
+			interaction.reply(`Setup Complete. The server owner ${user.username} receive the role ${roleName}`);
 			return;
 		}
+
 		interaction.reply('This command can only used by guild owner');
 	}
 };
--- src/Discord/repositories/roleManager.repository.js
+++ src/Discord/repositories/roleManager.repository.js
@@ -2,37 +2,42 @@
  *
  * @param guild - interaction.Guild
  * @param roleName the name of discord role
- * @param args
+ * @param permissions
  * @param role_color
  * @returns {Promise<*>}
  * @constructor
  */
-async function CreateRole(guild, roleName, args, role_color){
+async function CreateRole(guild, roleName, permissions, role_color){
 	return guild.roles.create({
 		name: roleName,
 		color: role_color,
 		mentionable: true,
-		permissions: args
+		permissions
 	});
 
 }
 
-async function FetchRoleInGuild(guild, roleName){
+function ExistsRoleInGuild(guild, roleName){
 	return !!guild.roles.cache.find(role => role.name === roleName);
 }
 
+function FetchRoleInGuild(interaction, roleName){
+	return interaction.guild.roles.cache.find(({name}) => name === roleName);
+}
+
 async function AssignRoleToUser(interaction, roleName){
-	const role_ = interaction.guild.roles.cache.find(role => role.name === roleName).id;
+	const role = FetchRoleInGuild(interaction, roleName);
+
 	const member = await interaction.guild.members.fetch(interaction.user.id);
-	await member.roles.add(role_);
+	await member.roles.add(role.id);
 }
 async function RemoveUserRole(interaction, roleName){
-	const role_ = interaction.guild.roles.cache.find(role => role.name === roleName).id;
+	const role = FetchRoleInGuild(interaction, roleName);
 
-	if(interaction.user && !interaction.user.roles.cache.has(role_)){
-		await interaction.user.roles.remove(role_);
+	if (interaction.user && !interaction.user.roles.cache.has(role.id)) {
+		await interaction.user.roles.remove(role.id);
 	}
 }
 
 
-module.exports = { CreateRole, FetchRoleInGuild, AssignRoleToUser, RemoveUserRole };
+module.exports = { CreateRole, ExistsRoleInGuild, AssignRoleToUser, RemoveUserRole, FetchRoleInGuild };
