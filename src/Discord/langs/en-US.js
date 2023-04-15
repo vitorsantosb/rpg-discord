405c422a8a2c627372d8bad3a4233d1790855f87
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2023-04-15 10:28:06 -0300

    Update: all commands text created

--- src/Discord/langs/en-US.js
+++ src/Discord/langs/en-US.js
@@ -1,25 +1,93 @@
-module.exports ={
+const {GetFullUsername} = require('../services/user.service');
+module.exports = {
 	lang: 'en-US',
 	commands: {
 		setup: {
 			name: 'setup',
 			description: 'Config the bot in your server - Obs: This function is enabled  for owner server!',
 
-			labels:{
+			labels: {
 				command_setup_cancel: 'Cancel',
 				command_setup_accept: 'Agree'
 			},
-			warning:{
+			warning: {
 				title: 'WARNING',
 				description: (roles) => `This command requires a set of role names.\n They are: ${roles} \n Do you Accept ?`,
 			},
 			reply: 'Setup your bot',
-			setupEvent:{
+			setupEvent: {
 				isSetup: 'Guild is already setup!',
 				setup_complete_reply: (user, ownerRoleName) => `Setup Complete!!. The server owner ${user.username} receive the role ${ownerRoleName} Good Game!!`,
 				cancel_setup: 'Operation canceled'
 			}
-
 		},
+		register: {
+			name: 'register',
+			description: 'register your user in guild, this is necessary for create a rpg session',
+
+			register_reply: {
+				registered: 'Already Registered!',
+				success: 'Register  Successfully!',
+			}
+		},
+		bodydice: {
+			name: 'bodydice',
+			description: 'Randomizer a body part',
+
+			bodyParts: [
+				'Left Arm',
+				'Right Arm',
+				'Left Leg',
+				'Right Leg',
+				'Spine',
+				'Head',
+				'Thorax'
+			],
+			reply: 'HIT: '
+		},
+		rolls: {
+			name: 'roll',
+			description: 'Dice Rolls',
+
+			option: {
+				description: 'Select the dice',
+				modifier: 'Add modifier for your dice!',
+				rolls: 'Number of rolls for your dice. Ex: 2d20, 2d10',
+				min_value: 'Minimal value for get a success in your dice!'
+			},
+			reply: 'Dice result: '
+		},
+		setGameMaster: {
+			name: 'set_gamemaster',
+			description: 'setup game master with mention',
+
+			option:{
+				mentionable: {
+					description: 'Set one user for a rpg game master'
+				},
+				boolean: {
+					description: 'is GameMaster ?'
+				},
+			},
+			reply: {
+				game_master_complete: (user, interaction) => `This user ${GetFullUsername(user)} now is new RPG master by ${interaction.user.username + '#' + interaction.user.discriminator}`,
+			}
+		},
+		addSessionMember: {
+			name: 'add_session_member',
+			description: 'Add one user in session with mention',
+
+			option:{
+				mentionable: {
+					description: 'member for add in session'
+				},
+				boolean: {
+					description: 'Session name'
+				},
+			},
+			reply_message: (user, sessionName, interaction) => `New member ${GetFullUsername(user)} added on session ${sessionName} by GameMaster ${interaction.user.username + '#' + interaction.user.discriminator}`,
+			reply_fail: 'Session does not exists',
+		}
+
 	}
 };
